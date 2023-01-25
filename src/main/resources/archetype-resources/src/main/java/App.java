2b637b870dbbd4ec1c150cb9dcec35f18ed0f768
fxstud <45392855+fxstud@users.noreply.github.com>
2023-01-25 10:58:54 +0100

    Update App.java

--- src/main/resources/archetype-resources/src/main/java/App.java
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello the World! is beautiful" );
+        System.out.println( "Hello the World!" );
     }
 }
